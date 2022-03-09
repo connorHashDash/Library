12c515edf04c16d26f9da482acfabceafe8c6e58
connorHashDash <ckb1997@hotmail.co.uk>
2022-03-09 22:43:37 +0000

    Can now toggle whether you've read the book or not

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let log = console.log
 
-
 let j = 0; // to cycle the "myLibrary" array by one
 
 const mainDiv = document.getElementById('main')
@@ -16,55 +15,17 @@ const formPages = document.getElementById('pages')
 const form = document.getElementById('form')
 const formSubmit = document.getElementById('formSubmit')
 
+
 //Data from form
 
 let myLibrary = []
-
-function bookDisplay() {
-  for(i = 0; i < myLibrary.length; i++){
-    let bookDiv = document.createElement('div') 
-    bookDiv.className = `bookDiv`
-    mainDiv.appendChild(bookDiv)
-    let currentBook = myLibrary[i]
-    let title = document.createElement('p')
-    title.className = `title`
-    title.innerHTML = `Title: ${currentBook.title}`
-    bookDiv.appendChild(title)
-    let author = document.createElement('p')
-    author.className = `author`
-    author.innerHTML = `Author: ${currentBook.author}`
-    bookDiv.appendChild(author)
-    let pages = document.createElement('p')
-    pages.className = `pages`
-    pages.innerHTML = `Pages: ${currentBook.pages}`
-    bookDiv.appendChild(pages)
-    let read = document.createElement('p')
-    read.className = `read`
-    read.innerHTML = `Read: ${currentBook.read}`
-    bookDiv.appendChild(read)
-    let removeButton = document.createElement('button')
-    removeButton.innerHTML = `Remove`
-    removeButton.className = `RemoveButton`
-    bookDiv.appendChild(removeButton)
-    removeButton.addEventListener('click', function(){
-      this.parentElement.remove()
-    });
-    let readButton = document.createElement('Button')
-    readButton.innerHTML = `Read`
-    readButton.className = `readButton`
-    bookDiv.appendChild(readButton)
-    readButton.addEventListener('click', function(){
-      this.currentBook.readChange()
-    });
-  }
-}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   let inputTitle = document.getElementsByName('title')[0].value
   let inputAuthor = document.getElementsByName('author')[0].value
-  let inputpage = document.getElementsByName('pages')[0].value
-  myLibrary[j] = new Book(inputTitle, inputAuthor, inputpage)
+  let inputPage = document.getElementsByName('pages')[0].value
+  myLibrary[j] = new Book(inputTitle, inputAuthor, inputPage)
   j++;
   bookDisplay();
 });
@@ -117,5 +78,53 @@ function Book(title, author, pages){
       this.read.innerHTML = 'Read: no';
       this.read = false;
     }
+  }
+}
+
+function bookDisplay() {
+  for(i = 0; i < myLibrary.length; i++){
+    let bookDiv = document.createElement('div') 
+    bookDiv.className = `bookDiv`
+    mainDiv.appendChild(bookDiv)
+    let currentBook = myLibrary[i]
+    let title = document.createElement('p')
+    title.className = `title`
+    title.innerHTML = `Title: ${currentBook.title}`
+    bookDiv.appendChild(title)
+    let author = document.createElement('p')
+    author.className = `author`
+    author.innerHTML = `Author: ${currentBook.author}`
+    bookDiv.appendChild(author)
+    let pages = document.createElement('p')
+    pages.className = `pages`
+    pages.innerHTML = `Pages: ${currentBook.pages}`
+    bookDiv.appendChild(pages)
+    let read = document.createElement('p')
+    read.className = `read`
+    read.innerHTML = `Read: ${currentBook.read}`
+    bookDiv.appendChild(read)
+    let removeButton = document.createElement('button')
+    removeButton.innerHTML = `Remove`
+    removeButton.className = `RemoveButton`
+    bookDiv.appendChild(removeButton)
+    removeButton.addEventListener('click', function(){
+      this.parentElement.remove()
+    });
+    let readButton = document.createElement('Button')
+    readButton.innerHTML = `Read`
+    readButton.className = `readButton`
+    bookDiv.appendChild(readButton)
+    readButton.addEventListener('click', function(){
+    let readStatus = false
+      if (read.innerHTML == 'Read: false'){
+        read.innerHTML = '';
+        read.innerHTML = 'Read: yes';
+        readStatus = true
+      } else {
+        read.innerHTML = '';
+        read.innerHTML = 'Read: false';
+        readStatus = false
+      }
+    });
   }
 }
