92e5523aebc830e8135979b5c599d711538f8a5e
connorHashDash <ckb1997@hotmail.co.uk>
2022-03-12 20:05:43 +0000

    info populating the books is now accurate

--- app.js
+++ app.js
@@ -15,75 +15,78 @@ const formPages = document.getElementById('pages')
 const form = document.getElementById('form')
 const formSubmit = document.getElementById('formSubmit')
 
-
 //Data from form
 
-let myLibrary = []
+let myLibrary = [];
 
+let x = 0;
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  let inputTitle = document.getElementsByName('title')[0].value
-  let inputAuthor = document.getElementsByName('author')[0].value
-  let inputPage = document.getElementsByName('pages')[0].value
-  myLibrary[j] = new Book(inputTitle, inputAuthor, inputPage)
-  j++;
-  bookDisplay();
+    e.preventDefault()
+    let inputTitle = document.getElementsByName('title')[0].value
+    let inputAuthor = document.getElementsByName('author')[0].value
+    let inputPage = document.getElementsByName('pages')[0].value
+    myLibrary[j] = new Book(inputTitle, inputAuthor, inputPage)
+    form.reset();
+    j++;
+    x++;
+    log(x);
+    bookDisplay();
 });
 
 openFormButton.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
 })
 
 closeFormButton.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modalForm')
-    closeModal(modal)
-  })
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modalForm')
+        closeModal(modal)
+    })
 })
 
 overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modalForm.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
+    const modals = document.querySelectorAll('.modalForm.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
 })
 
-function openModal(modal){
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
 }
 
-function closeModal(modal){
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
 }
 
-function Book(title, author, pages){
-  this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.read = 'no',
-  this.readChange = function(){
-    if (this.read == false){
-      this.read.innerHTML = '';
-      this.read.innerHTML = 'Read: yes';
-      this.read = true;
-    } else {
-      this.read.innerHTML = '';
-      this.read.innerHTML = 'Read: no';
-      this.read = false;
-    }
-  }
+function Book(title, author, pages) {
+    this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.read = 'no',
+        this.readChange = function() {
+            if (this.read == false) {
+                this.read.innerHTML = '';
+                this.read.innerHTML = 'Read: yes';
+                this.read = true;
+            } else {
+                this.read.innerHTML = '';
+                this.read.innerHTML = 'Read: no';
+                this.read = false;
+            }
+        }
 }
 
+    let i = 0;
 function bookDisplay() {
-let i = 0;
-    let bookDiv = document.createElement('div') 
+    let bookDiv = document.createElement('div')
     bookDiv.className = `bookDiv`
     mainDiv.appendChild(bookDiv)
 
@@ -112,25 +115,25 @@ let i = 0;
     removeButton.innerHTML = `Remove`
     removeButton.className = `RemoveButton`
     bookDiv.appendChild(removeButton)
-    removeButton.addEventListener('click', function(){
-      this.parentElement.remove()
+    removeButton.addEventListener('click', function() {
+        this.parentElement.remove()
     });
-
+    i++
+  log(`i= ${i}`)
     let readButton = document.createElement('Button')
     readButton.innerHTML = `Read`
     readButton.className = `readButton`
     bookDiv.appendChild(readButton)
-    readButton.addEventListener('click', function(){
-
-    let readStatus = false
-      if (read.innerHTML == 'Read: no'){
-        read.innerHTML = '';
-        read.innerHTML = 'Read: yes';
-        readStatus = true
-      } else {
-        read.innerHTML = '';
-        read.innerHTML = 'Read: no';
-        readStatus = false
-      }
+    readButton.addEventListener('click', function() {
+        let readStatus = false
+        if (read.innerHTML == 'Read: no') {
+            read.innerHTML = '';
+            read.innerHTML = 'Read: yes';
+            readStatus = true
+        } else {
+            read.innerHTML = '';
+            read.innerHTML = 'Read: no';
+            readStatus = false
+        }
     });
-  }
+}
